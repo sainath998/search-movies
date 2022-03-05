bb2860310140791060a4bdcf009d68db7205735a
sainath <sainathreddy863@gmail.com>
2022-03-05 13:49:32 +0530

    added google signin

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 
+// import firebase sdk
+// install firebase react-firebae hooks
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/analytics";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  // your config
+  apiKey: "AIzaSyA_8qtj_XbY2jwD1n-NVlZXi4iYggHs-II",
+  authDomain: "watch-movies-42cb9.firebaseapp.com",
+  projectId: "watch-movies-42cb9",
+  storageBucket: "watch-movies-42cb9.appspot.com",
+  messagingSenderId: "247948017732",
+  appId: "1:247948017732:web:9c62bd45e69bbe9d4b3c11",
+  measurementId: "G-4R798697SB",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
 const API_URL = "https://www.omdbapi.com?apikey=b177d325";
 
-const movie1 = {
-  Title: "Batman Begins",
-  Year: "2005",
-  imdbID: "tt0372784",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-};
-
 function App() {
+  const [user] = useAuthState(auth);
+
   // b177d325
   const serachMovies = async (title) => {
     if (title == "") title = "Spider";
@@ -39,28 +57,60 @@ function App() {
   }, [searchTerm]);
 
   return (
-    <div className="app">
-      <h1>Movie World</h1>
+    <div>
+      {user && (
+        <div className="app">
+          <h1>Movie World</h1>
+          <div className="search">
+            <input
+              placeholder="search for movies"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <img src={logo} />
+          </div>
+          <SignOut />
+          {movies?.length > 0 ? (
+            <div className="container">
+              {/* <MovieCard movie1={movie1} /> */}
+              {movies?.map((movie) => (
+                <MovieCard key={movie.imdbID} movie1={movie} />
+              ))}
+            </div>
+          ) : (
+            <div>
+              <h2>No Movies Found</h2>
+            </div>
+          )}
+        </div>
+      )}
+      {!user && <SignIn />}
+    </div>
+  );
+}
 
-      <div className="search">
-        <input
-          placeholder="search for movies"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <img src={logo} />
-      </div>
-      {movies?.length > 0 ? (
-        <div className="container">
-          {/* <MovieCard movie1={movie1} /> */}
-          {movies?.map((movie) => (
-            <MovieCard key={movie.imdbID} movie1={movie} />
-          ))}
-        </div>
-      ) : (
-        <div>
-          <h2>No Movies Found</h2>
-        </div>
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <div>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
+
+function SignOut() {
+  return (
+    <div>
+      {auth.currentUser && (
+        <button className="sign-out" onClick={() => auth.signOut()}>
+          Sign Out
+        </button>
       )}
     </div>
   );
